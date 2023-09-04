586555ee6bb42aadbad03858f040f44454b17047
sadhik-a <sadhik@bititude.com>
2023-09-04 15:49:27 +0530

    added authorization middleware

--- routes/employee.js
+++ routes/employee.js
@@ -1,16 +1,10 @@
 const express = require('express');
-const { body } = require('express-validator');
+
 const employeecontrol = require('../controllers/employee.js');
-
 const router = express.Router();
-
+const middleware = require('../utils/authmiddleware.js');
 router.get('/employee', employeecontrol.getemployees);
-router.post('/employee',  employeecontrol.addemployee);
-router.post('/login',
-  body('email').isEmail(),
-  body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long'),
-  employeecontrol.login
-);
-router.delete('/employee/:id', employeecontrol.deleteemployee);
-router.put('/employee/:id', employeecontrol.updateemployee);
+router.post('/employee', middleware.verification, employeecontrol.addemployee);
+router.delete('/employee/:id', middleware.verification ,employeecontrol.deleteemployee);
+router.put('/employee/:id', middleware.verification ,employeecontrol.updateemployee);
 module.exports = router;
