7691ab94c0cda82cf2de743c1e95e8de1329ad84
sadhik-a <sadhik@bititude.com>
2023-09-04 15:50:31 +0530

    user login & regn added

---
+++ routes/user.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const usercontrol = require('../controllers/user.js');
+const { body, validationResult } = require('express-validator');
+const router = express.Router();
+
+router.post(
+  '/register',
+  [
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
+  ],
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    usercontrol.register(req, res);
+  }
+);
+
+
+router.post(
+  '/login',
+  [
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
+  ],
+  async (req, res) => {
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    usercontrol.jwtlogin(req, res);
+  }
+);
+
+module.exports = router;
