72b73281c58f25a6bab335d8e2d07f56eddd25f6
sadhik-a <sadhik@bititude.com>
2023-09-04 15:57:06 +0530

    added user routes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
- const express = require('express');
+
+ require('dotenv').config(); 
+const express = require('express');
  const employeeroutes = require('./routes/employee.js');
+ const userroutes=require('./routes/user.js')
  const app = express();
  app.use(express.json());
+ app.use(userroutes);
  app.use(employeeroutes);
  app.listen(3000 , () => {
    console.log("Listening on port 3000");
